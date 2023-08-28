7ab546ff19c2d4dfa6ad5bedc9844efb760b8faa
SandraS2611 <sssandra1980@gmail.com>
2023-08-28 09:21:08 -0300

    funciones y desestructuracion

--- main.js
+++ main.js
@@ -1,70 +1,102 @@
-let nombre = "Sandra ";
-let apellido = "12"; //IGUAL LO CONCATENA AUNQ SEA OTRO TIPO
+// //Funciones
+// // let num1 = 10;
+// // let num2 = 5;
+// // let num3 = 10;
+// // let num4 = 20;
 
-//console.log(nombre, apellido);
+// // let operacion = "suma";
 
-let nombreCompleto = nombre + apellido;
-console.log(nombreCompleto);
+// // if (operacion == "suma") {
+// //   console.log(num1 + num2);
+// // } else if (operacion == "resta") {
+// //   console.log(num1 - num2);
+// // } else if (operacion == "multiplicacion") {
+// //   console.log(num1 * num2);
+// // } else if (operacion == "division") {
+// //   console.log(num1 / num2);
+// // } else {
+// //   console.log("Operacion no valida");
+// // }
 
-if (2 == "2") {
-  //el contenido es igual para JS
-  console.log("VERDAD");
-} else {
-  console.log("FALSO");
+// //Funciones Declarativa
+// function decirHola () {
+//     console.log("Hola mundo");
+// }
+// decirHola();
+// decirHola();
+// decirHola();
+
+// //Funcion Expresiva(NO SE USA)
+// // const decirHola2 = function () {
+// //     console.log("hola mundo2");
+// // }
+// // decirHola();
+// // decirHola2();
+
+// //ARROW FUNCTIONS - FUNCIONES DE FLECHA
+// const decirHola3 = () => {
+//     console.log("Hola mundo3");
+// }
+// decirHola();
+// decirHola2();
+// decirHola3();
+/////////////////////////////////////////////////////////
+
+// function suma (numero1 = 0, numero2 = 0) {
+//     console.log(numero1 + numero2);
+// }
+// suma(1, 2);
+// suma(6, 6);
+// suma(1);
+
+function calculadora(num1, num2, operacion) {// () = argumentos
+  if (operacion == "suma") {
+    console.log(num1 + num2);
+  } else if (operacion == "resta") {
+    console.log(num1 - num2);
+  } else if (operacion == "multiplicacion") {
+    console.log(num1 * num2);
+  } else if (operacion == "division") {
+    console.log(num1 / num2);
+  } else {
+    console.log("Operacion no valida");
+  }
 }
+calculadora(10, 20, "suma");// () = parametros
+calculadora(50, 30, "resta");
+calculadora(10, 20, "multiplicacion");
+calculadora(10, 20, "division");
+calculadora(10, 20);
+calculadora(10, "suma");
 
-if (2 === "2") {
-  //JS compara los tipos === es mas estricto
-  console.log("VERDAD");
-} else {
-  console.log("FALSO");
-}
-console.log(`${nombre} ${apellido}`);
 
-console.log(nombre.concat(apellido));
+function calculadora({ operacion, num1, num2 }) {// {} = define un objeto de argumento
+    if (operacion == "suma") {
+      console.log(num1 + num2);
+    } else if (operacion == "resta") {
+      console.log(num1 - num2);
+    } else if (operacion == "multiplicacion") {
+      console.log(num1 * num2);
+    } else if (operacion == "division") {
+      console.log(num1 / num2);
+    } else {
+      console.log("Operacion no valida");
+    }
+  }
+  calculadora({// {} = propiedades de los objetos del arg en los param
+    operacion: "resta",
+    num1: 10,
+  })
 
-let stringRandom = " jdkjhj djgjklj gkfjd ";
+  ///////////////////////////////////////////////
+  const miObjeto = {
+    nombre: "Sandra",
+    apellido: "Sanchez",
+  };
+  const miArray = ["Sandra", "Sanchez"];
 
-console.log(stringRandom);
+  //Desestructuracion de objetos
+  const {apellido, nombre} = miObjeto;
 
-console.log(stringRandom.trim()); //limpia el espacio de adelante y de atras
-
-console.log(stringRandom.split()); //convierte un string en un array de 1 posicion
-
-console.log(stringRandom.split("")); // separa los array en distintas partes
-
-console.log(stringRandom.trim().split(""));// no hay espacios con comillas
-
-let stringRandom1 = "123456";
-console.log(stringRandom1);
-
-console.log(stringRandom1.trim()); //limpia el espacio del principio y del final
-
-console.log(stringRandom1.split()); //convierte un string en un array de 1 posicion
-
-console.log(stringRandom1.split("")); // separa los array en distintas partes
-
-console.log(stringRandom1.trim().split(""));
-
-console.log(stringRandom1.trim().split("").reverse());//invierte el orden
-
-//reverse y join metodos de array
-console.log(stringRandom1.trim().split("").reverse().join(","));//los une y les agrega comillas para separaro puede ser un espacio
-
-//console
-console.log("mensaje");
-console.error("Error");
-console.warn("Warn");
-console.info("Info");
-
-let miArray = [1, 2, 3, 4, 5, 6]
-console.log(miArray);
-
-let miArray1 = [//array de 2D - matriz de 3x3
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-];
-console.log(miArray1);
-
-console.table(miArray1);
+  //Desestructuracion de arrays importa el orden
+  const [variable1, variable2] = miArray;
