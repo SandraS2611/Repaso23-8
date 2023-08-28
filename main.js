a983ebada5241ccf6f076f4868e6379155bca3a2
SandraS2611 <sssandra1980@gmail.com>
2023-08-28 08:33:35 -0300

    metodos comunes de strings y metodos de console

--- main.js
+++ main.js
@@ -1,254 +1,70 @@
-//Estructuras de control de Flujo Condicionales
+let nombre = "Sandra ";
+let apellido = "12"; //IGUAL LO CONCATENA AUNQ SEA OTRO TIPO
 
-if (3 > 4) {
-  //<--- SI, (VERDAD)
-  console.log("VERDADERO");
+//console.log(nombre, apellido);
+
+let nombreCompleto = nombre + apellido;
+console.log(nombreCompleto);
+
+if (2 == "2") {
+  //el contenido es igual para JS
+  console.log("VERDAD");
 } else {
-  // <--- NO, (FALSEDAD)
   console.log("FALSO");
 }
 
-if (3 > 3) {
-  //<--- SI, (VERDAD)
-  console.log("VERDADERO");
+if (2 === "2") {
+  //JS compara los tipos === es mas estricto
+  console.log("VERDAD");
 } else {
-  // <--- NO, (FALSEDAD)
   console.log("FALSO");
 }
+console.log(`${nombre} ${apellido}`);
 
-if (3 >= 2) {
-  //<--- SI, (VERDAD)
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+console.log(nombre.concat(apellido));
 
-if (3 >= 3) {
-  //<--- SI, (VERDAD)
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+let stringRandom = " jdkjhj djgjklj gkfjd ";
 
-if (4 >= 3) {
-  //<--- SI, (VERDAD)
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+console.log(stringRandom);
 
-if (2 >= 3) {
-  //<--- SI, (VERDAD)
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+console.log(stringRandom.trim()); //limpia el espacio de adelante y de atras
 
-//o - or = ||
-if (3 == 4 || 4 > 3) {
-  //<--- || = o , or
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+console.log(stringRandom.split()); //convierte un string en un array de 1 posicion
 
-// y - and = &&
-if (3 == 4 && 4 > 3) {
-  //<--- && = y, and
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+console.log(stringRandom.split("")); // separa los array en distintas partes
 
-//not = !
-console.log(3 == 4);
-console.log(!(3 == 4));
+console.log(stringRandom.trim().split(""));// no hay espacios con comillas
 
-if (!(3 == 4)) {
-  // <--- NOT = !
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+let stringRandom1 = "123456";
+console.log(stringRandom1);
 
-if (!(3 == 3)) {
-  // <--- NOT = !
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+console.log(stringRandom1.trim()); //limpia el espacio del principio y del final
 
-if (!!(3 == 4)) {
-  // <--- NOT = !
-  console.log("VERDADERO");
-} else {
-  // <--- NO, (FALSEDAD)
-  console.log("FALSO");
-}
+console.log(stringRandom1.split()); //convierte un string en un array de 1 posicion
 
-if (!(3 == 3)) {
-  // <--- NOT = !
-  console.log("CASO IF");
-} else if (3 == 2) {
-  // <--- NO, (FALSEDAD)
-  console.log("CASO ELSE IF");
-} else {
-  console.log("CASO POR DEFECTO");
-}
-//////////////////////////////////////////////////////////
-let condicion = 5; // CAMBIAR LOS VALORES( 13, 10,ETC)
+console.log(stringRandom1.split("")); // separa los array en distintas partes
 
-if (condicion > 12) {
-  console.log("CASO IF");
-} else if (condicion > 9) {
-  console.log("CASO ELSE IF");
-} else if (condicion > 7) {
-  console.log("CASO ELSE IF2");
-} else {
-  console.log("CASO POR DEFECTO");
-}
+console.log(stringRandom1.trim().split(""));
 
-//////////////////////////////////////////////////////
-//CALCULADORA SENCILLA
-let num1 = 10;
-let num2 = 5;
+console.log(stringRandom1.trim().split("").reverse());//invierte el orden
 
-let operacion = "suma";
+//reverse y join metodos de array
+console.log(stringRandom1.trim().split("").reverse().join(","));//los une y les agrega comillas para separaro puede ser un espacio
 
-if (operacion == "suma") {
-  console.log(num1 + num2);
-} else if (operacion == "resta") {
-  console.log(num1 - num2);
-} else if (operacion == "multiplicacion") {
-  console.log(num1 * num2);
-} else if (operacion == "division") {
-  console.log(num1 / num2);
-} else {
-  console.log("Operacion no valida");
-}
-///////////////////////////////////////////////////////
-let valor = 3;
+//console
+console.log("mensaje");
+console.error("Error");
+console.warn("Warn");
+console.info("Info");
 
-switch (valor) {
-  case 1:
-    console.log("valor es igual a 1");
-    break;
-  case 2:
-    console.log("valor es igual a 2");
-    break;
-  default:
-    console.log("Operacion no valida");
-    break;
-}
+let miArray = [1, 2, 3, 4, 5, 6]
+console.log(miArray);
 
-switch (operacion) {
-  case "suma":
-    console.log(num1 + num2);
-    break;
-  case "resta":
-    console.log(num1 - num2);
-    break;
-  case "multiplicacion":
-    console.log(num1 * num2);
-    break;
-  case "division":
-    console.log(num1 / num2);
-    break;
-  default:
-    console.log("Operacion no valida");
-    break;
-}
+let miArray1 = [//array de 2D - matriz de 3x3
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+console.log(miArray1);
 
-// var cualquierCosa = "hola";
-// if (true) {
-//   var cualquierCosa = "chau";
-//   console.log(cualquierCosa);
-// }
-
-// console.log(cualquierCosa);
-
-let cualquierCosa = "hola";
-if (true) {
-  let cualquierCosa = "chau";
-  console.log(cualquierCosa);
-}
-
-console.log(cualquierCosa);
-////////////////////////////////////////////////////
-
-// let iterador = 0
-// while (iterador < 10) {
-//     console.log("hola mundo");  // crea un bucle infinito
-//     iterador++;// es lo mismo q = + 1
-// }
-
-// let iterador = 0
-// while (iterador < 5) {
-//     console.log("hola mundo");  // crea un bucle infinito
-//     iterador++;// es lo mismo q = + 1
-// }
-
-// let iterador = 0;
-
-// let miArray = ["Chau", "Hello", "Bye", "Hi"];
-// console.log(miArray.length);
-
-// while (iterador < miArray.length) {
-//   console.log(miArray[iterador]);
-//   iterador++; // es lo mismo q = + 1
-// }
-
-// let whileIterador = 0;
-
-//let miArray2 = ["Chau", "Hello", "Bye", "Hi"];
-//console.log(miArray2.length);
-
-// while (whileIterador < miArray2.length) {
-//   console.log(miArray2[whileIterador]);
-//   whileIterador++; // es lo mismo q = + 1
-// }
-
-while (false) {
-  console.log("hello while");
-}
-
-let miArray2 = ["Chau", "Hello", "Bye", "Hi", "Log"];
-console.log(miArray2.length);
-///////////////////////////////////////////////////////////
-let i = 0;
-
-do {
-  console.log(miArray2[i]);
-  i++;
-} while (i < miArray2.length);
-
-////////////////////////////////////////////////////////
-for (let i = 0; i < miArray2.length; i++) {
-  //console.log("Bucle for: ", miArray2[i]);
-  console.log("for ", i);
-}
-///////////////////////////////////////////////////////
-if (3 == 3) {
-  true;
-} else {
-  false;
-} // NO DEVUELVE NADA PORQ ESTA DECLARANDO
-
-//----TERNARIA (? Y DECLARAR VALORES EN BASE A UNA COND)------
-
-// let miVariable = 3 == 3 ? "Hola" : "Chau";
-//    console.log(miVariable);
-
-//    let miVariable = 3 == 4 ? "Hola" : "Chau";
-//    console.log(miVariable);
-
-// console.log(3 == 4 ? "Hola" : "Chau");
-console.log(3 == 3 ? "Hola" : "Chau");
+console.table(miArray1);
